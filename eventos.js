5cf08f028b9210bfe9c9caa89a0c39a043b3fbe1
CeciliaSalinas <salinascecilia.mdl@gmail.com>
2024-07-09 12:44:54 -0300

    maquetado web

---
+++ eventos.js
@@ -0,0 +1,76 @@
+/*permita a un usuario escribir parte del titulo de un libro, 
+y mostrar una lista de todos los libros que coincidan con ese nombre, 
+
+hacer una página Web que le permita a un usuario escribir parte del nombre o el apellido de un autor, 
+y mostrar una lista de todos los autores que coincidan con ese nombre
+
+con la fecha de nacimiento y muerte del autor (si es que ambos datos están disponibles).
+Una vez mostrada la lista; al hacer clic en uno de los autores, deberemos mostrar la lista de obras de ese autor.
+*/
+
+function mostrar(){
+    document.getElementById('membresia').style.display = 'block';
+}
+
+
+//ej js asincrónico : buscar libros según titulo o palabra ingresada 
+//mostar una lista con esos titulos
+//Una vez mostrada la lista; al hacer clic en uno de los libros, deberemos mostrar la lista de obras de ese autor,
+// y entre paréntesis el año de publicación de cada obra.
+
+function buscarLibros(){
+
+    let titulo = document.querySelector('#buscarLibros').value;
+
+    if (titulo.length == 0){
+        return;
+    }
+    muestraInformacionEspera();
+
+    // Realizamos el fetch, que *retorna una promesa*, que se cumple cuando
+    // *comienza* a llegar la respuesta (encabezados). Esta segunda promesa
+    // retorna también una promesa, que se cumple con los datos de los autores,
+    // con los que invocamos a la función muestraLibros
+
+    fetch("https://openlibrary.org/search.json?title="+ titulo)
+    .then(Response => Response.json())
+    .then(datos =>{muestraLibros(datos, titulo); })
+    .catch(error => { console.log("ERROR"); console.log(error)});
+}
+
+function muestraInformacionEspera(){
+
+    // Deshabilitamos el botón para que no se realicen búsquedas simultáneas:
+    document.querySelector('#boton-buscar').disabled = true;
+    
+    // Ponemos un mensaje para indicar que estamos esperando la respuesta:
+    document.querySelector('#respuesta p').innerHTML = "Buscando titulos...";
+}
+
+function muestraLibros(datos, titulo){
+    document.querySelector('#respuesta').innerHTML =
+    '<p>Los títulos encontrados de ' + titulo +': <span id="cantidad"></span> </p>';
+
+    let lista = document.createElement('ul');
+    let item;
+
+    datos.docs.forEach( unDato => {
+        item = document.createElement('li');
+        item.innerHTML = unDato.title;
+        lista.appendChild(item);
+    });
+
+    document.querySelector('#respuesta').appendChild(lista);
+    document.querySelector('#cantidad').innerHTML = datos.numFound;
+    document.querySelector('#cantidad').innerHTML += "(mostrando "+ document.querySelectorAll('#respuesta ul li').length + ")";
+
+     // Volvemos a activar el botón de búsqueda:
+     document.querySelector('#boton-buscar').disabled = false;
+}
+
+
+
+
+
+
+
---
+++ suscripcion.php
@@ -0,0 +1,146 @@
+
+El nombre debe tener al menos 3 caracteres, ninguno de ellos numérico.
+El apellido debe tener al menos 2 caracteres, ninguno de ellos numérico.
+Validar que se haya elegido alguna de las opciones del radio button.
+Validar que el mail no esté vacío.
+Validar que el campo de cuotas es un número.
+
+
+El script debe calcular el precio de la suscripción. 
+El precio base de la misma es de $1000, y aplican las siguientes modificaciones:
+Si la cantidad de cuotas es 1, tiene un 5% de descuento.
+Si la cantidad de cuotas del pago es mayor a 6, tiene un 10% de recargo.
+Si es fuera de nuestra provincia, tiene $200 de recargo.
+Si es estudiante, tiene un 15% de descuento.
+Todos los descuentos y recargos se aplican sobre el precio base.
+El script debe mostrar el precio base, todos los descuentos y recargos a aplicar, y el precio final.
+
+<?php
+
+//guardarlos valores que recibo por POST
+$nombre = $_POST ['nombre'];
+$apellido = $_POST['apellido'];
+$email = $_POST['email'];
+$provincia = $_POST['provincia'];
+$mensaje = $_POST['mensaje'];
+
+ //isset:  función que verifica si una variable está definida y no es nula. es un valor booleano
+ //verifica si el campo 'estudiante' existe dentro del array $_POST.
+$estudiante = isset($_POST['estudiante']);
+
+// Si $_POST['libros'] no existe o es vacío, guardamos la cadena vacía. Si no,
+// guardamos el valor recibido
+$libros = empty($_POST['libros']) ? '' : $_POST['libros'];
+
+//validamos errores
+$errores = revisarErrores($nombre, $apellido, $libros, $cuotas);
+
+//si no hubo errores calculo respuesta
+if (count($errores)==0){
+    $respuesta = calcularPrecios($cuotas, $provincia, $estudiante);
+}
+
+//agrego los errores a la respuesta
+$respuesta['errores'] = $errores;
+
+//configurar la respuesta HTTP para que sea interpretada correctamente como un objeto JSON y luego enviar dicho objeto JSON al cliente.
+header('Content-Type: application/json; charset=utf-8');
+
+//json_encode() convierte un valor de PHP (como un array o un objeto) en una cadena JSON.
+//$respuesta es la variable que contiene los datos que deseas convertir a JSON.
+echo json_encode($respuesta);
+
+/**
+ * Revisa que los valores recibidos por POST pasen las validaciones:
+ *
+ * @ return array Retorna un array con los errores (puede estar vacío si no hay
+ *               errores).
+ */
+
+function revisarErrores($nombre, $apellido, $libros, $cuotas){
+
+    $errores = [];
+
+     //En este caso, strlen($nombre) calcula la longitud de la cadena almacenada en la variable $nombre.
+    if (strlen($nombre) < 3){
+        $errores[] = "Error: el nombre debe tener al menos tres letras";
+    }
+
+    if (strlen($apellido) < 2){
+        $errores[] = "Error: el apellido debe tener al menos dos letras";
+    }
+
+    if (tiene_numeros($nombre)) {
+        $errores[] = "Error: El nombre contiene números";
+    }
+    if (tiene_numeros($apellido)) {
+        $errores[] = "Error: El nombre contiene números";
+    }
+
+    //verifica si una variable está vacía. Retorna true si la variable no existe,
+    //El operador de negación lógica ! invierte el resultado de empty().
+    if (!empty($_POST['libros'])) {
+        $tipo = $_POST['libros'];
+    } else {
+        $errores[] = "Error: debe elegir un tipo de formato de libro";
+    }
+
+    if (!is_numeric($cuotas) || $cuotas <= 0) {
+        $errores[] = "Error: la cantidad de cuotas debe ser un nro > 0";
+    }
+
+    return $errores
+}
+
+
+    /**
+ * Esta función calcula el precio de la suscripción.
+ * @ return Array Retorna un array que contiene el precio base, el total de
+ *               recargos, el total de descuentos y el precio final.
+ */
+
+ function calcular_precios($provincia, $cuotas, $es_estudiante)
+ {
+     $descuento = 0;
+     $recargo = 0;
+     $precio_base = 1000;
+     $descuento_contado = 0.05;
+     $max_cuotas_sin_recargo = 6;
+     $recargo_muchas_cuotas = 0.1;
+     $recargo_envio = 200;
+     $descuento_estudiante = 0.15;
+ 
+     if ($cuotas == 1) {
+         $descuento += $precio_base * $descuento_contado;
+     }
+     if ($provincia != 's') {
+         $recargo += $recargo_envio;
+     }
+     if ($cuotas > $max_cuotas_sin_recargo) {
+         $recargo += $precio_base * $recargo_muchas_cuotas;
+     }
+     if ($es_estudiante) {
+         $descuento += $precio_base * $descuento_estudiante;
+     }
+     $respuesta['precio_base'] = $precio_base;
+     $respuesta['descuento'] = $descuento;
+     $respuesta['recargo'] = $recargo;
+     $respuesta['total'] = $precio_base + $recargo - $descuento;
+ 
+     return $respuesta;
+ }
+ 
+/**
+ * Esta función verifica si la cadena recibida por parámetro tiene dígitos o no.
+ * @ param string $texto El texto a analizar
+ * @ return boolean true si tiene dígitos, false si no los tiene.
+ */
+function tiene_numeros($texto) {
+    for ($i = 0; $i < strlen($texto); $i++) {
+        if (is_numeric($texto[$i])) {
+            return true;
+        }
+    }
+    return false;
+}
+<?
